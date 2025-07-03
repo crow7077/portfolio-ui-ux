ee6c3bcd83e3c76a156c4cef0ed42f95ce6f3de6
Renato Nunes Junior <renatonj0489@gmail.com>
2025-07-02 22:45:51 -0300

    atualizado 3

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -6,6 +6,11 @@ import cv from "../assets/images/cv.pdf";
 import arrow from "../assets/images/arrow.svg";
 import foldercv from "../assets/images/folder.svg";
 import letraT from "../assets/images/letra-t-vetorizada.png";
+import notebook from "../assets/images/notebook.png";
+import project from "../assets/images/project-img.png";
+import phone from "../assets/images/phone.svg";
+import paintBucket from "../assets/images/paint-bucket.svg";
+import vetor from "../assets/images/vetor.svg";
 
 const Container = () => {
   return (
@@ -22,8 +27,21 @@ const Container = () => {
       />
       <img className="arrow" src={arrow} alt="Seta Clicando" />
       <img className="letra-t" src={letraT} alt="Letra T" />
-      <div className="yellowbox"></div>
-      <div className="yellowbox2"></div>
+      <div className="line-horizon"></div>
+      <div className="yellowbox">
+        <h1 className="h02">Identidade Visual</h1>
+        <h1 className="h01">Identidade Visual</h1>
+        <h1 className="h03">Identidade Visual</h1>
+        <div className="line-vert1"></div>
+        <div className="line-vert2"></div>
+      </div>
+      <div className="yellowbox2">
+        <div>
+          <img className="phone1" src={phone} alt="Phone" />
+          <img className="phone2" src={phone} alt="Phone" />
+          <img className="phone3" src={phone} alt="Phone" />
+        </div>
+      </div>
       <div className="typewrite">Design UI/UX</div>
       <h1 className="welcome">
         BEM VINDO
@@ -34,6 +52,46 @@ const Container = () => {
         <br />
         DE
       </h1>
+      <div>
+        <img className="notebook1" src={notebook} alt="Notebook" />
+        <img className="notebook2" src={notebook} alt="Notebook" />
+        <div className="box-white1"></div>
+        <div className="box-white2"></div>
+      </div>
+      <div className="projects1">
+        <img className="project1" src={project} alt="cards" />
+        <img className="project1" src={project} alt="cards" />
+        <img className="project1" src={project} alt="cards" />
+        <img className="project1" src={project} alt="cards" />
+      </div>
+      <div className="projects2">
+        <img className="project1" src={project} alt="cards" />
+        <img className="project1" src={project} alt="cards" />
+        <img className="project1" src={project} alt="cards" />
+        <img className="project1" src={project} alt="cards" />
+        <img className="project1" src={project} alt="cards" />
+      </div>
+      <div className="info-final">
+        <img className="paint" src={paintBucket} alt="paint" />
+        <div className="box-yellow"></div>
+        <h1>
+          MEU ESTILO DE
+          <br />
+          <span className="design-h1">DESIGN</span> É UM
+          <br />
+          REFLEXO DA MINHA
+          <br />
+          PERSONALIDADE.
+        </h1>
+      </div>
+      <div className="contact">
+        <img className="vetor" src={vetor} alt="vetor" />
+        <h1>
+          ENTRE EM
+          <br />
+          CONTATO
+        </h1>
+      </div>
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "../styles/Footer.css"
+import footer from "../assets/images/img-footer.svg";
 
 const Footer = () => {
-    return <div className="footer">{
-      //conteudo do footer
-  }</div>;
+  return (
+    <div className="footer">
+      <img className="footer-img" src={footer} alt="Footer" />
+    </div>
+  );
 };
 
 export default Footer;
