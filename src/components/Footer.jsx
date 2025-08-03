9d61d3b01021c8e41bbc470d16ccb3adf3528f61
Renato Nunes Junior <renatonj0489@gmail.com>
2025-08-03 19:41:57 -0300

    responsive 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import "../styles/Footer.css"
+import React from "react";
+import "../styles/Footer.css";
 import footer from "../assets/images/img-footer.svg";
 import homeIcon from "../assets/images/home.svg";
+import whatsapp from "../assets/icons/whatsapp.svg";
+import gitHub from "../assets/icons/github.svg";
+import email from "../assets/icons/email.svg";
+import instagram from "../assets/icons/instagram.svg";
+import linkedin from "../assets/icons/linkedin.svg";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <img className="footer-img" src={footer} alt="Footer" />
-      <img className="home-icon" src={homeIcon} alt="Home Icon" />
+      <div>
+        <img className="footer-img" src={footer} alt="Footer" />
+        <img className="home-icon" src={homeIcon} alt="Home Icon" />
+      </div>
+      <div>
+        <h1>
+          Ficaria feliz em discutir novas oportunidades ou responder suas
+          perguntas.
+        </h1>
+        <div redes-sociais>
+          <div className="whatsapp">
+            <img src={whatsapp} alt="WhatsApp" />
+            <h2>WhatsApp</h2>
+          </div>
+          <div className="github">
+            <img src={gitHub} alt="GitHub" />
+            <h2>GitHub</h2>
+          </div>
+          <div className="email">
+            <img src={email} alt="E-mail" />
+            <h2>E-mail</h2>
+          </div>
+          <div className="instagram">
+            <img src={instagram} alt="Instagram" />
+            <h2>Instagram</h2>
+          </div>
+          <div className="linkedin">
+            <img src={linkedin} alt="LinkedIn" />
+            <h2>LinkedIn</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,34 @@
-import  '../styles/Header.css';
+import '../styles/Header.css';
+import nsDesign from "../assets/images/minha-logo.svg";
 
 const Header = () => {
        return (
-        <>
-            {/* Barra de Navegação Superior */}
-            <nav className="navbar">
-                <div className="navbarBrand">
-                    <div className="brandCircle"></div>
-                    <span className="brandName">NSDesign</span>
-                </div>
-                <ul className="navbarNav">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">Sobre mim</a></li>
-                    <li><a href="#projects">Meus Projetos</a></li>
-                    <li><a href="#contact">Contato</a></li>
-                </ul>
-            </nav>
-        </>
-    );
+         <>
+           {/* Barra de Navegação Superior */}
+           <nav className="navbar">
+             <div className="navbarBrand">
+               <div className="brandCircle">
+                 <img className="ns-design" src={nsDesign} alt="Logo" />
+               </div>
+               <span className="brandName">NSDesign</span>
+             </div>
+             <ul className="navbarNav">
+               <li>
+                 <a href="#home">Home</a>
+               </li>
+               <li>
+                 <a href="#about">Sobre mim</a>
+               </li>
+               <li>
+                 <a href="#projects">Meus Projetos</a>
+               </li>
+               <li>
+                 <a href="#contact">Contato</a>
+               </li>
+             </ul>
+           </nav>
+         </>
+       );
 };
 
 export default Header;
